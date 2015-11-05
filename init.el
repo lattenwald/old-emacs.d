aa2807d5eb757c16f59c4622679d9856c416623f
Alexander Kyusev <a.kyusev@corp.mail.ru>
2015-11-05 10:27:24 +0300

    docker stuff

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
 
      ;; packages
      ag company company-ghc cperl-mode exec-path-from-shell diminish
+     dockerfile-mode docker
      fixmee flx-ido flycheck go-mode haskell-mode helm helm-ag idomenu
      multi-term projectile smart-tabs-mode smex spaceline
      sunrise-commander sr-speedbar undo-tree vimish-fold weechat
