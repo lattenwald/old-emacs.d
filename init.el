8686916b073d65119f51ff9011962476e9407775
Alexander Kyusev <a.kyusev@corp.mail.ru>
2015-12-03 11:55:15 +0300

    added lua-mode

--- init.el
+++ init.el
@@ -11,11 +11,10 @@
 
      ;; packages
      ag company company-ghc cperl-mode elm-mode exec-path-from-shell
-     diminish dockerfile-mode docker
-     fixmee flx-ido flycheck go-mode haskell-mode helm helm-ag idomenu
-     multi-term projectile smart-tabs-mode smex spaceline
-     sunrise-commander sr-speedbar undo-tree vimish-fold weechat
-     yaml-mode
+     diminish dockerfile-mode docker fixmee flx-ido flycheck go-mode
+     haskell-mode helm helm-ag idomenu lua-mode multi-term projectile
+     smart-tabs-mode smex spaceline sunrise-commander sr-speedbar
+     undo-tree vimish-fold weechat yaml-mode
      )
   "Packages whose location follows the
   packages/package-name/package-name.el format.")
