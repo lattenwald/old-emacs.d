657ea0c24d67777012bd55398f99462be85d35ee
Alexander Kyusev <a.kyusev@corp.mail.ru>
2015-12-09 13:14:09 +0300

    misc settings

--- custom.el
+++ custom.el
@@ -24,6 +24,7 @@
  '(custom-safe-themes
    (quote
     ("73abbe794b6467bbf6a9f04867da0befa604a072b38012039e8c1ba730e5f7a5" "8eaa3bce3c618cd81a318fcf2d28c1cd21278531f028feb53186f6387547dfb4" "a4f8d45297894ffdd98738551505a336a7b3096605b467da83fae00f53b13f01" "af9761c65a81bd14ee3f32bc2ffc966000f57e0c9d31e392bc011504674c07d6" default)))
+ '(default-input-method "cyrillic-jcuken")
  '(delete-old-versions t)
  '(dired-listing-switches "-al --group-directories-first")
  '(electric-pair-mode t)
@@ -88,7 +89,7 @@
  '(projectile-global-mode t)
  '(projectile-globally-ignored-directories
    (quote
-    (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elm-stuff")))
+    (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elm-stuff" "node_modules")))
  '(projectile-mode-line (quote (:eval (format " P[%s]" (projectile-project-name)))))
  '(shm-auto-insert-bangs t)
  '(shm-auto-insert-skeletons t)
